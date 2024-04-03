7a11be15e0a089d9b49afd24acdb71fdd9e66629
Arthur <arthur.lucas92@orange.fr>
2024-04-03 12:09:26 +0200

    Scroll down button in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import "./App.css";
+import "./App.scss";
 import { useState, useEffect } from "react";
-import ArthurW from "./ARTHURS-WHITE.svg";
-import PortfolioTitleW from "./PORTFOLIO-WHITE.svg";
 
 function App() {
   return (
     <div>
       <header>
+        <div className="topHeader"></div>
         <div className="cube-container">
-          <div className="cube">
+          <div className="cube1">
             <div className="front">ArtHur</div>
             <div className="back">ArtHur</div>
             <div className="top">poRtfolio</div>
             <div className="bottom">poRtfolio</div>
+          </div>
+        </div>
+        <div className="scrollDown">
+          <div className="cube-scroll">
+            <div className="front"></div>
+            <div className="back"></div>
+            <div className="top"></div>
+            <div className="bottom"></div>
+            <div className="right"></div>
+            <div className="left"></div>
           </div>
         </div>
       </header>
